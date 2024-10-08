8dd6b24519d44f79547f692c8e484a29ee2b14bd
murodbek_08 <murodbekarslonov201@gmail.com>
2024-10-08 12:31:13 +0500

    O'zgartirish haqida qisqa izoh

--- js/main.js
+++ js/main.js
@@ -48,3 +48,15 @@ function shrink() {
     navbar.classList.remove("navbar-shrink");
   }
 }
+
+// dark-mode
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "./images/sun.svg";
+  } else {
+    this.firstElementChild.src = "./images/moon.svg";
+  }
+  document.body.classList.toggle("dark");
+});
